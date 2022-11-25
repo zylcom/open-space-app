f8dbfdd4c131a72d888db33b92c53e217582b379
Sabilillah <sabilillah.dev@gmail.com>
2022-11-25 13:34:40 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./states";
+import App from "./App";
+
+import "./styles/style.css";
+
+const root = createRoot(document.getElementById("root"));
+
+// TODO: wrap App with store provider
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>,
+);
